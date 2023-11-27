df3fce021c4affb85ed77ffe7191b9fbdef2a366
Karolina Zhabo <karolinazhabo@gmail.com>
2023-11-27 11:35:54 +0100

    init fix git easy

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("________________-");
         System.out.println("change");
+        System.out.println("Git as easy");
     }
 }
